bbfe8e83126f083117d71802a8bd06ebfe9a2558
yelizabethla <46800257+yelizabethla@users.noreply.github.com>
2020-03-07 00:58:04 -0500

    updated with javaScript
    price changes based the the quantity of items the user wants to purchase.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 
-// starter code for week 5 of pui lab
+// update price based on the quantity of item the customer wants to buy
 
 
-function addNewList() {
-    alert('hello world alert!');
-    console.log('hello world console');
+function updatePrice() {
+    var input = Number(document.getElementById('quantity').value);
+    var currentlist = document.getElementById('product-price').innerHTML.split(" ");
+    var current = Number(currentlist[1]);
+    var update = String(input*current)
+    document.getElementById('product-price').innerHTML = "$ " + update;
 }
-
